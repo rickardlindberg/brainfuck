a3434c3377e4fef7d0487cf7ab784275a50a92e8
Rickard Lindberg <ricli85@gmail.com>
2012-06-14 11:00:26 +0200

    clean up

--- versions/bitetail/Brainfuck.hs
+++ versions/bitetail/Brainfuck.hs
@@ -42,31 +42,8 @@ data Instruction
     | Loop [Instruction]
     deriving (Show, Eq)
 
-newParse :: String -> [Instruction]
-newParse input = let ("", instructios) = parseBlock input [] in instructios
-
 parse :: String -> [Instruction]
-parse [] = []
-parse (x:xs)
-    | x == ']'          = error "unexpected ]"
-    | x == '['          = parseLoop xs
-    | x `elem` "+-<>.," = parseSingle x : parse xs
-    | otherwise         =                 parse xs
-
-parseLoop :: String -> [Instruction]
-parseLoop input = let (innerLoop, rest) = newParseInnerLoop input
-                      loop              = Loop (innerLoop ++ loop) : (parse rest)
-                  in loop
-
-newParseInnerLoop :: String -> ([Instruction], String)
-newParseInnerLoop x = let (part, rest) = inner 0 x in (parse part, rest)
-    where
-        inner n []     = error "unexpected end of input"
-        inner n (x:xs)
-            | n == 0 && x == ']' = ([], xs)
-            |           x == ']' = let (x', xs') = inner (n-1) xs in (x:x', xs')
-            |           x == '[' = let (x', xs') = inner (n+1) xs in (x:x', xs')
-            |          otherwise = let (x', xs') = inner n     xs in (x:x', xs')
+parse input = let ("", instructios) = parseBlock input [] in instructios
 
 parseBlock :: String -> [Instruction] -> (String, [Instruction])
 parseBlock []                      opTail = ([], opTail)
@@ -89,21 +66,17 @@ parseSingle '.' = Print
 parseSingle ',' = Read
 
 run :: [Instruction] -> String -> Data -> String
-run []             input  dat = "done!\n"
-run (Inc:next)     input  dat = run next input (dataModifyValue dat (+1))
-run (Dec:next)     input  dat = run next input (dataModifyValue dat (\x -> x - 1))
-run (Left:next)    input  dat = run next input (dataMoveLeft dat)
-run (Right:next)   input  dat = run next input (dataMoveRight dat)
-run (Print:next)   input  dat = chr (dataGet dat) : run next input dat
-run (Read:next)    []     dat = error "no input"
-run (Read:next)    (i:is) dat = run next is (dataModifyValue dat (const (ord i)))
+run []               input  dat = "done!\n"
+run (Inc:next)       input  dat = run next input (dataModifyValue dat (+1))
+run (Dec:next)       input  dat = run next input (dataModifyValue dat (\x -> x - 1))
+run (Left:next)      input  dat = run next input (dataMoveLeft dat)
+run (Right:next)     input  dat = run next input (dataMoveRight dat)
+run (Print:next)     input  dat = chr (dataGet dat) : run next input dat
+run (Read:next)      []     dat = error "no input"
+run (Read:next)      (i:is) dat = run next is (dataModifyValue dat (const (ord i)))
 run ((Loop xs):next) input  dat = if dataGet dat == 0
-                                    then run next input dat
-                                    else run xs input dat
+                                      then run next input dat
+                                      else run xs input dat
 
 execute :: String -> IO ()
-execute program = interact (\input -> run (newParse program) input emptyData)
-
--- $ ./dist/Main test_programs/echo_until_q.bf
--- abcd
--- Main: versions/bitetail/Brainfuck.hs:(77,1)-(87,57): Non-exhaustive patterns in function Brainfuck.run
+execute program = interact (\input -> run (parse program) input emptyData)
